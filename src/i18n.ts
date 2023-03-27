7eb3697a5909e69f4a0ea6ab9cbcf26384a4d2d5
bosom <1245924849@qq.com>
2023-03-27 21:20:36 +0800

    first init

---
+++ src/i18n.ts
@@ -0,0 +1,36 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+import Backend from 'i18next-http-backend';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+export const i18nLanguages = [
+  // 'ar',
+  'da',
+  'en',
+  'es',
+  'ja',
+  'ms',
+  'nb',
+  'sv',
+  'zh-CN',
+  'zh-HK',
+  'zh-TW',
+];
+
+i18n
+  .use(Backend)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    backend: {
+      loadPath: 'locales/{{lng}}/{{ns}}.json',
+    },
+    fallbackLng: {
+      default: ['en'],
+    },
+    ns: ['main', 'api', 'about', 'model'],
+    defaultNS: 'main',
+  });
+
+export default i18n;
---
+++ src/store/auth-slice.ts
@@ -0,0 +1,34 @@
+import { defaultAPIEndpoint } from '@constants/auth';
+import { StoreSlice } from './store';
+
+export interface AuthSlice {
+  apiKey?: string;
+  apiEndpoint: string;
+  firstVisit: boolean;
+  setApiKey: (apiKey: string) => void;
+  setApiEndpoint: (apiEndpoint: string) => void;
+  setFirstVisit: (firstVisit: boolean) => void;
+}
+
+export const createAuthSlice: StoreSlice<AuthSlice> = (set, get) => ({
+  apiEndpoint: defaultAPIEndpoint,
+  firstVisit: true,
+  setApiKey: (apiKey: string) => {
+    set((prev: AuthSlice) => ({
+      ...prev,
+      apiKey: apiKey,
+    }));
+  },
+  setApiEndpoint: (apiEndpoint: string) => {
+    set((prev: AuthSlice) => ({
+      ...prev,
+      apiEndpoint: apiEndpoint,
+    }));
+  },
+  setFirstVisit: (firstVisit: boolean) => {
+    set((prev: AuthSlice) => ({
+      ...prev,
+      firstVisit: firstVisit,
+    }));
+  },
+});
